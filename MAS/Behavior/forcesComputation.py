Fix some formula errors in van der walls computation

MAS/Behavior/forcesComputation.py
@@ -28,16 +28,16 @@ class ForcesComputation :
 
             """absolute temperature = 0K or -273.15°C or -459.67°F"""
             absolute_temperature = -273.15
-            e_keesom = ((agent.dipole_moment ** 2)*(p.dipole_moment ** 2))/(3 *((4*const.pi*const.epsilon_0 * agent.environment.relative_permittivity) ** 2) * const.k * absolute_temperature)
-            e_debye = (((agent.dipole_moment ** 2) * p.polarizability) + ((p.dipole_moment ** 2)*agent.polarizability))/(4 * const.pi * const.epsilon_0 * agent.environment.relative_permittivity)
+            e_keesom = ((agent.dipole_moment ** 2) * (p.dipole_moment ** 2))/(3 * ((4 * const.pi * const.epsilon_0 * agent.environment.relative_permittivity) ** 2) * const.k * absolute_temperature)
+            e_debye = (((agent.dipole_moment ** 2) * p.polarizability) + ((p.dipole_moment ** 2) * agent.polarizability))/((4 * const.pi * const.epsilon_0 * agent.environment.relative_permittivity)**2)
 
             electronic_absorption_frequency = 10
             """ICI : Pas encore trouvé ce que c'est vraiment"""
 
-            e_london = (
-                           const.Planck * electronic_absorption_frequency * agent.polarizability * p.polarizability) / (
-                           4 * const.pi * const.epsilon_0 * agent.environment.relative_permittivity)
-            vdw = (- 1 / (np.linalg.norm(agent.position - p.position) ** 6)) * (e_keesom + e_debye + e_london)
+            e_london = (3/4)*(
+                           const.Planck * electronic_absorption_frequency * agent.polarizability * p.polarizability) / ((
+                           4 * const.pi * const.epsilon_0)**2)
+            vdw = (- 1/(np.linalg.norm(agent.position - p.position)**6)) * (e_keesom + e_debye + e_london)
 
             unit_vector = (agent.position - p.position) / np.linalg.norm(agent.position - p.position)
             interactions.append(vdw * unit_vector)